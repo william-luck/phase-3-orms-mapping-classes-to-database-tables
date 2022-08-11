b5796be272c12c95efee0c0bcd1318e37f547cd4
William Luck <luck.william.h@gmail.com>
2022-08-11 12:25:01 -0500

    commit tree 449a6c315eed83c974842b8864c0487129cf2782

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,6 @@ Bundler.require
 
 require_relative '../lib/song'
 
+# All this line is doing is setting up a constant that is eqeula to a hash that contains the connection to the database.
+# Can access the constant and teh connection through DB[:conn]
 DB = { conn: SQLite3::Database.new("db/music.db") }
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,51 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :name, :album, :id
 
-  def initialize(name:, album:)
+  # Set the id equal to nil because that will be the responsibility of the database.
+  def initialize(name:, album:, id: nil)
     @name = name
     @album = album
+    @id = id
   end
+
+  # crafts an SQL statement, responsibility of the class, so class method
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+
+  # Usually a good idea not to include this method upon intialization, and to keep them separate. 
+  def save
+    sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.album)
+
+    # Gets the song ID from the database, save it to the Ruby instance.
+    self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+
+    # returns Ruby instance
+    self
+
+  end
+
+  def self.create(name:, album:)
+    song = Song.new(name: name, album: album)
+    song.save 
+  end
+
+
+
+
+
 
 end
